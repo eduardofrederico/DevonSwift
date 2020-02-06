c1cfedba167ae92ced12a8b84c5321a796a75458
Eduardo Frederico <eduardo.frederico@icloud.com>
2020-02-06 20:43:01 -0300

    Meus primeiros conhecimentos em Swift.
    Criei uma função onde o texto digitado no TextField, aparece no Label quando o Button é pressionado.

---
+++ DevonSwift/FirstViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FirstViewController.swift
+//  DevonSwift
+//
+//  Created by Eduardo on 06/02/20.
+//  Copyright © 2020 Eduardo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func IdTapButton(_ sender: Any) {
+        nameLabel.text = nameTextField.text;    }
+}
